aa5d14aed2977bb4a6ed6022d383d45bfd35f931
Jonas Thiel <jt@neopoly.de>
2016-09-16 16:44:31 +0200

    Add test infrastructure

--- lib/redmine-mattermost.rb
+++ lib/redmine-mattermost.rb
@@ -1,6 +1,9 @@
 require "redmine_mattermost/version"
 require "redmine_mattermost/infos"
-require "redmine_mattermost/engine"
+
+if defined?(Rails)
+  require "redmine_mattermost/engine"
+end
 
 # Mattermost chat integration
 module RedmineMattermost
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'redmine-mattermost'
+
+require 'minitest/autorun'
+require 'webmock/minitest'
